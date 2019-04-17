1e88442bd32391cd27e55583fa5adaf0d3fa0514
Molly Jean Bennett <molly@castironcoding.com>
2019-04-17 15:55:05 -0700

    [F] Implement custom footer logo component

---
+++ customComponents/src/components/Logo/index.js
@@ -0,0 +1,11 @@
+import UmnPress from "./UMNPress";
+import GraduateCenter from "./GraduateCenter";
+import GcInitiatives from "./GcInitiatives";
+import GcScholarship from "./GcScholarship";
+
+export default {
+  UmnPress,
+  GraduateCenter,
+  GcInitiatives,
+  GcScholarship
+};
