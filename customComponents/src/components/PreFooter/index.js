7d25d955a9b369c25a0a1125d5c8947c20217cee
Molly Jean Bennett <molly@castironcoding.com>
2019-04-23 11:39:26 -0700

    [F] Implement randomized chapter with sample data

--- customComponents/src/components/PreFooter/index.js
+++ customComponents/src/components/PreFooter/index.js
@@ -2,26 +2,46 @@ import React, { Component } from "react";
 import "./styles.scss";
 import thumbnail from "../../../assets/dhdebates_cover_thumb.jpg"
 import Utility from "global/components/utility";
+import chapters from "./chapters.json"
 
 export default class PreFooter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { chapter: chapters["01"]}
+  }
+
+  componentDidMount() {
+    const randomChapter = this.getChapter();
+    this.setState({ chapter: randomChapter})
+  }
+
+  getChapter() {
+    const chapterKeys = Object.keys(chapters);
+    const randomChapterKey = chapterKeys[Math.floor(Math.random() * chapterKeys.length)];
+    return chapters[randomChapterKey];
+  }
+
   render() {
-    const iconClass= "action-callout-list__button-icon"
+    const baseClass= "custom-prefooter";
+    const buttonClass = "action-callout-list";
+    const { chapter } = this.state;
+
     return (
-      <section className="custom-prefooter ">
-        <div className="container custom-prefooter__flex-container">
-          <figure className="custom-prefooter__figure">
-            <img className="custom-prefooter__thumbnail" alt="" src={thumbnail} />
+      <section className={`${baseClass} `} >
+        <div className={`container ${baseClass}__flex-container`}>
+          <figure className={`${baseClass}__figure`}>
+            <img className={`${baseClass}__thumbnail`} alt="" src={thumbnail} />
           </figure>
           <div className="">
-            <a className="custom-prefooter__header-link" href="">
-              <h4 className="custom-prefooter__heading heading-primary">Alien Reading: Text Mining, Language Standardization, and the Humanities</h4>
+            <a className={`${baseClass}__header-link`} href={chapter.link}>
+              <h4 className={`${baseClass}__heading heading-primary`}>{chapter.heading}</h4>
             </a>
             <div className="">
-              <p className="custom-prefooter__body-text">Jeffrey M. Binder argues that, when applied to the study of literary and cultural texts, statistical text-mining methods tend to reinforce conceptions of language and meaning that are, at best, overly dependent on the “literal” definitions of words and, at worst, complicit in the marginalization of nonstandard linguistic conventions and modes of expression.</p>
-              <div className="action-callout-list action-callout-list--inline custom-prefooter__button-container">
-                <a className="action-callout-list__button--primary action-callout-list__button" href="">
-                  <Utility.IconComposer icon="glasses64" size={46} iconClass={iconClass} />
-                  <span className="action-callout-list__button-text">Read this Chapter</span>
+              <p className={`${baseClass}__body-text`}>{chapter.description}</p>
+              <div className={`${buttonClass} ${buttonClass}--inline ${baseClass}__button-container`}>
+                <a className={`${buttonClass}__button--primary ${buttonClass}__button`} href={chapter.link}>
+                  <Utility.IconComposer icon="glasses64" size={46} iconClass={`${buttonClass}__button-icon`} />
+                  <span className={`${buttonClass}__button-text`}>Read this Chapter</span>
                 </a>
               </div>
             </div>
